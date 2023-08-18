cfc6ca7f467d82fa99c66448337103219b8a0adc
bengris32 <bengris32@protonmail.ch>
2023-08-18 23:22:41 +0100

    teei: Disable TEEI log thread when !CONFIG_MICROTRUST_TZ_LOG
    * Hogs an incredible amount of CPU power when it's just not needed.
    
    Change-Id: Ic04391c4e234165c009f3cad3f1049458685dcd5
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    (cherry picked from commit be629926416023b03c5af522a5e788b26c31cb3d)
    (cherry picked from commit c56a0aeea26bbd3ab9b17232813b0cb37cbb2d51)

--- drivers/misc/mediatek/teei/400/tz_driver/teei_client_main.c
+++ drivers/misc/mediatek/teei/400/tz_driver/teei_client_main.c
@@ -1128,6 +1128,7 @@ static int teei_client_init(void)
 	sched_setscheduler_nocheck(teei_bdrv_task, SCHED_FIFO, &param);
 	wake_up_process(teei_bdrv_task);
 
+#ifdef CONFIG_MICROTRUST_TZ_LOG
 	init_tlog_comp_fn();
 
 /*
@@ -1141,6 +1142,7 @@ static int teei_client_init(void)
 	}
 
 	wake_up_process(teei_log_task);
+#endif
 
 	IMSG_DEBUG("create the sub_thread successfully!\n");
 */
