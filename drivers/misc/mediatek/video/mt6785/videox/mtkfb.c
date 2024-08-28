33ae89b44e8622e14ec67468eeb3bd15ec6840d2
bengris32 <bengris32@protonmail.ch>
2024-08-28 11:59:49 +0100

    drivers: mediatek: video: Fix switching to DC mode when AAL is disabled
    * When DC dimming is enabled, the brightness is set to the value of
      oplus_backlight_backup. With AAL disabled, this causes the backlight
      to turn off completely.
    
    * oplus_backlight_backup isn't updated when the display brightness changes,
      since that will only happen if AAL is supported. Make sure that oplus_backlight_backup
      is updated regardless of AAL support.
    
    Change-Id: Ie3f6249ecddb391393c822f0bec7246d9d614bec
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    (cherry picked from commit 6cd9bc63c8f52dff472bae639e3f480d2edc68d8)
    (cherry picked from commit fec2e1050322b50145b4c5d0f8b18e940c4696fd)
    (cherry picked from commit 114529c2be889e3ffdb9f2fb575f785f7e6fcc12)

--- drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6785/videox/mtkfb.c
@@ -362,16 +362,15 @@ int mtkfb_set_backlight_level(unsigned int level)
 	MTKFB_FUNC();
 
 	DISPDBG("%s:%d Start\n", __func__, level);
-
 	if (aal_is_support) {
-		#ifdef OPLUS_BUG_STABILITY
-		/* Zhijun.Ye@MM.Display.LCD.Stability 2020/11/16, Add for dc backlight*/
-		oplus_backlight_backup = level;
-		#endif /* OPLUS_BUG_STABILITY */
 		primary_display_setbacklight_nolock(level);
 	} else {
 		primary_display_setbacklight(level);
 	}
+
+	#ifdef OPLUS_BUG_STABILITY
+	oplus_backlight_backup = level;
+	#endif /* OPLUS_BUG_STABILITY */
 
 	DISPDBG("%s End\n", __func__);
 	return 0;
