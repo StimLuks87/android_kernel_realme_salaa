689571e3cc8a7ff1852740192769f01cc83081a7
Debayan Kar <kardebayan3@gmail.com>
2025-08-28 21:07:30 +0530

    build.sh: Update from RMX2020
    (cherry picked from commit 91ca9f94d66f32bdf9aaec43df29471df4ab8a7e)
    (cherry picked from commit 56fcbda85e8b1d10eac90e5c5aec5a76588ca3c3)

--- build.sh
+++ build.sh
@@ -1,81 +1,34 @@
-#!/bin/env bash
+#!/bin/bash
 
-# Define compile function
-function compile() {
-  # Load environment variables
-  source ~/.bashrc
-  source ~/.profile
+function compile() 
+{
+rm -rf AnyKernel
+source ~/.bashrc && source ~/.profile
+TANGGAL=$(date +"%Y%m%d-%H")
+export LC_ALL=C && export USE_CCACHE=1
+export ARCH=arm64
+if [ ! -d "clang" ]; then
+    git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git clang --depth=1
+fi
 
-  # Set environment variables
-  export LC_ALL=C
-  export USE_CCACHE=1
-  export ARCH=arm64
-  export SUBARCH=arm64
+[ -d "out" ] && rm -rf out || mkdir -p out
 
-  export KBUILD_BUILD_HOST="PrathamsBuilds"
-  export KBUILD_BUILD_USER="Pratham"
+make O=out ARCH=arm64 salaa_defconfig
 
-  # Allocate 100GB of memory to ccache
-  ccache -M 100G
-
-  # Download clang if not present
-  if [[ ! -d "clang" ]]; then
-    aria2c -x16 -s16 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz
-    mkdir -p clang
-    tar -xf clang-r498229b.tar.gz -C clang/
-    rm -f clang-r498229b.tar.gz
-  fi
-
-  # Clean and create output directory
-  if [[ -d "out" ]]; then
-    rm -rf out/
-    mkdir -p "out"
-  fi
-
-  # Build the kernel
-  make -j$(nproc --all) O=out ARCH=${ARCH} SUBARCH=${SUBARCH} salaa_defconfig
-
-  # Add clang bin directory to PATH
-  PATH="${PWD}/clang/bin:${PATH}"
-
-  # Build the kernel with clang and log output
-  make -j$(nproc --all) O=out CC="clang" LLVM=1 CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee build.log
+PATH="${PWD}/clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      CC="clang" \
+                      LLVM=1 \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee build.log
 }
 
-# Define upload file function
-function upload_file() {
-  local filepath="$1"
-  link=$(curl --upload-file "$filepath" https://free.keep.sh/)
-  echo "$link"
+function zipping()
+{
+git clone --depth=1 https://github.com/Luks-organization/AnyKernel3.git AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 Bolt-ZAP-salaa-${TANGGAL}.zip *
 }
 
-# Define upload function
-function upload() {
-  # Check if kernel image exists
-  if [[ ! -f "out/arch/${ARCH}/boot/Image.gz-dtb" ]]; then
-    echo "ERROR: No kernel image found! Exiting now..."
-    upload_file "build.log"
-    exit 1
-  fi
-
-  # Clean and clone AnyKernel repository
-  if [[ -d "AnyKernel" ]]; then
-    rm -rf AnyKernel/
-  fi
-  git clone --single-branch --depth 1 -b salaa https://github.com/prathamdby/AnyKernel3.git AnyKernel
-
-  # Copy image to AnyKernel and zip the package
-  cp "out/arch/${ARCH}/boot/Image.gz-dtb" AnyKernel/
-  cd AnyKernel/
-  local date=$(date +'%d%m%y')
-  local file="Bolt-ZAP!-Kernel-V1-salaa-$date-release.zip"
-  zip -r9 "$file" *
-
-  # Upload the AnyKernel package
-  upload_file "$file"
-}
-
-# Run functions
 compile
-upload
-
+zipping
