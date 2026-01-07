2e907f6d66bc66faa35ffbb9bf97480c1be74708
StimLuks87 <san10031987san@gmail.com>
2026-01-07 08:11:22 +0500

    android: ion: mtk: fix clang -Wvoid-pointer-to-int-cast
    Signed-off-by: StimLuks87 <san10031987san@gmail.com>
    (cherry picked from commit c5970a9d14dc83a2c144f9f1a0340f563622c672)
    (cherry picked from commit 45957c940d037ec0095e74bdcabdbf4270dfa025)
    (cherry picked from commit 26d1672d705bc485176e3403e1aff511042fbd39)

--- drivers/staging/android/ion/mtk/ion_sec_heap.c
+++ drivers/staging/android/ion/mtk/ion_sec_heap.c
@@ -319,7 +319,7 @@ ion_fd2sec_type(int fd, int *sec, int *iommu_sec_id,
 		ion_phys_addr_t *sec_hdl)
 {
 	enum TRUSTED_MEM_REQ_TYPE tmem_type = -1;
-	struct ion_buffer *buffer;
+	struct ion_buffer *buffer __maybe_unused;
 	struct ion_client *client;
 	struct ion_handle *handle;
 
