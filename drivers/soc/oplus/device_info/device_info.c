8fd5dbad25c3e5290b23fc4934fcd536e82ac4f7
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2026-02-19 13:33:44 +0500

    drivers: soc: oplus: Fix clang warnings. Signed-off-by: StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
    (cherry picked from commit 1c37f6d2b939475e887e464feaf7c9d260f76afa)

--- drivers/soc/oplus/device_info/device_info.c
+++ drivers/soc/oplus/device_info/device_info.c
@@ -250,7 +250,7 @@ EXPORT_SYMBOL(register_device_proc);
 static ssize_t fork_para_monitor_read_proc(struct file *file, char __user *buf,
                 size_t count, loff_t *off)
 {
-        char page[256] = {0};
+	    char page[256] __maybe_unused = {0};
         int ret = 0;
         /* ret = snprintf(page, 255, " times:%d\n father pid:%d\n child pid:%d\n",
 		happend_times, fork_pid_father, fork_pid_child); 
@@ -263,7 +263,7 @@ struct file_operations fork_para_monitor_proc_fops = {
         .write = NULL,
 };
 
-static void recursive_fork_para_monitor(void)
+static void __maybe_unused recursive_fork_para_monitor(void)
 {
 		struct proc_dir_entry *pentry;
 
@@ -642,7 +642,7 @@ reinit_aboard_id(struct device *dev, struct manufacture_info *info)
 	int i = 0, ret = 0;
 	int id_size = 0;
 	uint32_t *main_val = NULL, *sub_val = NULL, *rf_val = NULL;
-	int active_val = 0, sleep_val = 0, idle_val = 0;
+	int active_val = 0, sleep_val = 0, idle_val __maybe_unused = 0;
 	struct device_info *dev_info = g_dev_info;
 	bool match = false;
 
