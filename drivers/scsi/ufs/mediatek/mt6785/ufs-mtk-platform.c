42cd6a8f1398f15e058126f8e8c02b2e48b01df1
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2026-02-19 14:14:53 +0500

    drivers: scsi: ufs: suppress unused variable warnings in mt6785 platform Signed-off-by: StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
    (cherry picked from commit b8c508f839b3e651c3df50468a7f2e0fbdfabfcb)

--- drivers/scsi/ufs/mediatek/mt6785/ufs-mtk-platform.c
+++ drivers/scsi/ufs/mediatek/mt6785/ufs-mtk-platform.c
@@ -243,7 +243,7 @@ void ufs_mtk_pltfrm_gpio_trigger(int value)
 
 int ufs_mtk_pltfrm_xo_ufs_req(struct ufs_hba *hba, bool on)
 {
-	u32 value;
+	u32 value __maybe_unused;
 
 	if (!hba->card) {
 		/*
@@ -357,7 +357,7 @@ int ufs_mtk_pltfrm_bootrom_deputy(struct ufs_hba *hba)
 
 int ufs_mtk_pltfrm_ref_clk_ctrl(struct ufs_hba *hba, bool on)
 {
-	struct ufs_mtk_host *host = ufshcd_get_variant(hba);
+	struct ufs_mtk_host *host __maybe_unused = ufshcd_get_variant(hba);
 	int ret = 0;
 	u32 val = 0;
 
